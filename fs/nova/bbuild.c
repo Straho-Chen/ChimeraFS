c34549eba0d141fb83223fae9397e05bc446bb75
strahochen <strahochen@foxmail.com>
2024-12-21 13:53:38 +0800

    to(fs): lost journal.h for bbuild.c

--- fs/nova/bbuild.c
+++ fs/nova/bbuild.c
@@ -31,6 +31,7 @@
 #include <linux/signal.h>
 #include <linux/kernel.h>
 #include "nova.h"
+#include "journal.h"
 
 void nova_init_header(struct super_block *sb,
 		      struct nova_inode_info_header *sih, u16 i_mode)
