7c3776c8354e9a41ad16fe7000feb2786deea34a
strahochen <strahochen@foxmail.com>
2025-05-11 09:26:47 +0800

    to(blk-size): test and plot script

---
+++ eval/blk_size/test.sh
@@ -0,0 +1,190 @@
+#!/usr/bin/bash
+
+set -ex
+
+source "../tools/common.sh"
+
+sudo -v
+
+ABS_PATH=$(where_is_script "$0")
+TOOLS_PATH=$ABS_PATH/../tools
+
+# FS=("ext4-dax" "ext4-raid" "nova" "pmfs" "winefs")
+FS=("nova")
+
+# DELEGATION_FS=("parfs" "odinfs" "idel")
+DELEGATION_FS=("parfs")
+
+# UFS=("madfs")
+
+# in MB
+# TOTAL_FILE_SIZE=$((32 * 1024))
+TOTAL_FILE_SIZE=$((1 * 1024))
+
+NUM_JOBS=(1)
+
+# in B
+BLK_SIZES=($((4 * 1024)) $((8 * 1024)) $((16 * 1024)) $((32 * 1024)))
+
+DEL_THRDS=(12)
+
+TABLE_NAME="$ABS_PATH/performance-comparison-table"
+
+table_create "$TABLE_NAME" "fs ops filesz blksz numjobs bandwidth(MiB/s)"
+
+loop=1
+if [ "$1" ]; then
+    loop=$1
+fi
+
+fpath="/mnt/pmem0/"
+
+do_fio() {
+    local fs=$1
+    local op=$2
+    local fsize=$3
+    local bsz=$4
+    local job=$5
+    local del_thrds=$6
+    local numa_node=$7
+
+    if [[ "$op" == "write" || "$op" == "randwrite" || "$op" == "overwrite" || "$op" == "randoverwrite" ]]; then
+        grep_sign="WRITE:"
+    else
+        grep_sign="READ:"
+    fi
+
+    echo "FIO: $fs $op $fsize $bsz $job" >/dev/kmsg
+
+    fs_raw=$fs
+
+    # if $fs fall in delegation fs
+    if [[ "${DELEGATION_FS[@]}" =~ "$fs" ]]; then
+        bash "$TOOLS_PATH"/mount.sh "$fs" "$del_thrds"
+        fs=$fs-$del_thrds
+    else
+        bash "$TOOLS_PATH"/mount.sh "$fs"
+    fi
+
+    if [[ "$op" == "overwrite" ]]; then
+        cmd="$TOOLS_PATH/fio.sh $fpath $bsz $fsize $job write 1"
+    elif [[ "$op" == "randoverwrite" ]]; then
+        cmd="$TOOLS_PATH/fio.sh $fpath $bsz $fsize $job randwrite 1"
+    else
+        cmd="$TOOLS_PATH/fio.sh $fpath $bsz $fsize $job $op"
+    fi
+
+    if [[ -n "$numa_node" ]]; then
+        BW=$(numactl -N "$numa_node" $cmd | grep "$grep_sign" | awk '{print $2}' | sed 's/bw=//g' | "$TOOLS_PATH"/converter/to_MiB_s)
+    else
+        BW=$(bash $cmd | grep "$grep_sign" | awk '{print $2}' | sed 's/bw=//g' | "$TOOLS_PATH"/converter/to_MiB_s)
+    fi
+
+    bash "$TOOLS_PATH"/umount.sh "$fs_raw"
+
+    table_add_row "$TABLE_NAME" "$fs $op $fsize $bsz $job $BW"
+    fs=$fs_raw
+}
+
+# for normal fs
+
+for fs in "${FS[@]}"; do
+    compile_fs "$fs" "0"
+    for bsz in "${BLK_SIZES[@]}"; do
+        for job in "${NUM_JOBS[@]}"; do
+            fsize=($(("$TOTAL_FILE_SIZE" / "$job")))
+            for ((i = 1; i <= loop; i++)); do
+
+                do_fio "$fs" "write" "$fsize" "$bsz" "$job"
+                do_fio "$fs" "randwrite" "$fsize" "$bsz" "$job"
+                do_fio "$fs" "overwrite" "$fsize" "$bsz" "$job"
+                do_fio "$fs" "randoverwrite" "$fsize" "$bsz" "$job"
+                do_fio "$fs" "read" "$fsize" "$bsz" "$job"
+                do_fio "$fs" "randread" "$fsize" "$bsz" "$job"
+
+            done
+        done
+    done
+done
+
+# for delegation fs
+
+for fs in "${DELEGATION_FS[@]}"; do
+    for del_thrds in "${DEL_THRDS[@]}"; do
+        for bsz in "${BLK_SIZES[@]}"; do
+            for job in "${NUM_JOBS[@]}"; do
+                fsize=($(("$TOTAL_FILE_SIZE" / "$job")))
+                for ((i = 1; i <= loop; i++)); do
+
+                    if [[ "$fs" == "idel" ]]; then
+                        compile_fs "idel-low-thread" "0"
+                        do_fio "$fs" "write" "$fsize" "$bsz" "$job" "$del_thrds" "1"
+                        do_fio "$fs" "randwrite" "$fsize" "$bsz" "$job" "$del_thrds" "1"
+                        do_fio "$fs" "overwrite" "$fsize" "$bsz" "$job" "$del_thrds" "1"
+                        do_fio "$fs" "randoverwrite" "$fsize" "$bsz" "$job" "$del_thrds" "1"
+                    elif [[ "$fs" == "parfs" ]]; then
+                        compile_fs "low-thread" "0"
+                        do_fio "$fs" "write" "$fsize" "$bsz" "$job" "$del_thrds" "1"
+                        do_fio "$fs" "randwrite" "$fsize" "$bsz" "$job" "$del_thrds" "1"
+                        do_fio "$fs" "overwrite" "$fsize" "$bsz" "$job" "$del_thrds" "1"
+                        do_fio "$fs" "randoverwrite" "$fsize" "$bsz" "$job" "$del_thrds" "1"
+                        compile_fs "$fs" "0"
+                        do_fio "$fs" "read" "$fsize" "$bsz" "$job" "$del_thrds"
+                        do_fio "$fs" "randread" "$fsize" "$bsz" "$job" "$del_thrds"
+                    else
+                        compile_fs "$fs" "0"
+                        do_fio "$fs" "write" "$fsize" "$bsz" "$job" "$del_thrds"
+                        do_fio "$fs" "randwrite" "$fsize" "$bsz" "$job" "$del_thrds"
+                        do_fio "$fs" "overwrite" "$fsize" "$bsz" "$job" "$del_thrds"
+                        do_fio "$fs" "randoverwrite" "$fsize" "$bsz" "$job" "$del_thrds"
+                        do_fio "$fs" "read" "$fsize" "$bsz" "$job" "$del_thrds"
+                        do_fio "$fs" "randread" "$fsize" "$bsz" "$job" "$del_thrds"
+                    fi
+
+                done
+            done
+        done
+    done
+done
+
+# do_fio_ufs() {
+#     local fs=$1
+#     local op=$2
+#     local fsize=$3
+#     local bsz=$4
+#     local job=$5
+#     local ulib_path=$6
+
+#     if [[ "$op" == "write" || "$op" == "randwrite" ]]; then
+#         grep_sign="WRITE:"
+#     else
+#         grep_sign="READ:"
+#     fi
+
+#     echo "FIO: $fs $op $fsize $bsz $job" >/dev/kmsg
+
+#     bash "$TOOLS_PATH"/mount.sh "ext4-dax"
+
+#     BW=$(bash LD_PRELOAD=$ulib_path "$TOOLS_PATH"/fio.sh "$fpath" "$bsz" "$fsize" "$job" "$op" | grep "$grep_sign" | awk '{print $2}' | sed 's/bw=//g' | "$TOOLS_PATH"/converter/to_MiB_s)
+
+#     bash "$TOOLS_PATH"/umount.sh "ext4-dax"
+
+#     table_add_row "$TABLE_NAME" "$fs $op $fsize $bsz $job $BW"
+# }
+
+# for fs in "${UFS[@]}"; do
+#     ulib_path=$(ufs_lib_path "$fs")
+#     for bsz in "${BLK_SIZES[@]}"; do
+#         for job in "${NUM_JOBS[@]}"; do
+#             fsize=($(("$TOTAL_FILE_SIZE" / "$job")))
+#             for ((i = 1; i <= loop; i++)); do
+
+#                 do_fio_ufs "$fs" "write" "$fsize" "$bsz" "$job" "$ulib_path"
+#                 do_fio_ufs "$fs" "read" "$fsize" "$bsz" "$job" "$ulib_path"
+#                 do_fio_ufs "$fs" "randwrite" "$fsize" "$bsz" "$job" "$ulib_path"
+#                 do_fio_ufs "$fs" "randread" "$fsize" "$bsz" "$job" "$ulib_path"
+
+#             done
+#         done
+#     done
+# done
